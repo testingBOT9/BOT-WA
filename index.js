a02053139f592819f6f3c70cf405ce1b79f327b9
フェブリアン <75772930+testingBOT9@users.noreply.github.com>
2021-01-31 12:00:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-const { create, Client } = require('@open-wa/wa-automate');
-const figlet = require('figlet')
-const options = require('./utils/options')
-const { color, messageLog } = require(''./utils)
-const HandleMsg = require('./HandleMsg')
+const {
+    WaConnection
+    MessageType
+    Presence
+    Nimetype
+} = require('adiwajshing/Baileys')
 
-async functions start()=
-
-client.on('qr', (qr) => {
-
-    console.log('QR RECEIVED', qr);
-    qrcode.generate(qr);
-});
-
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
-
-client.on('message', msg => {
-    if (msg.body == '!ping') {
-        msg.reply('pong');
-    }
-});
-
-client.initialize();
