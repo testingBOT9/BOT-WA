49aae5c5c68fad3f7e2943a80fcc3a948e30d6de
フェブリアン <75772930+testingBOT9@users.noreply.github.com>
2021-01-31 12:08:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const {
     Presence,
     Mimetype
 } = require('adiwajshing/Baileys')
-
+const { color, bgcolor } = require('./lib/color.js')
+const { help } = require('./src/help.js')
+const { wait, simih, getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, banner, start } = require('./lib/functions')
